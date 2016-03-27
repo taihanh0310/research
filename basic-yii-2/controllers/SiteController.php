2ef4fcd164061f194b22cd040c76432cff73f3d7
Hanh Nguyen Tai <taihanh0310@gmail.com>
2016-03-27 23:15:08 +0700

    add new documents/yiibasic.pdf

--- basic-yii-2/controllers/SiteController.php
+++ basic-yii-2/controllers/SiteController.php
@@ -50,7 +50,7 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index');
+        return "fucker";//return $this->render('index');
     }
 
     public function actionLogin()

a8a3dea8b800cbf5e12cc100b06436d3b9ce0f32
Tai Hanh Nguyen <nthanh@IKORNSOLUTIONS.LOCAL>
2016-04-28 10:44:12 +0700

    console

--- basic-yii-2/config/console.php
+++ basic-yii-2/config/console.php
@@ -1,13 +1,13 @@
 <?php
 
-Yii::setAlias ('@tests', dirname (__DIR__) . '/tests/codeception');
+Yii::setAlias('@tests', dirname(__DIR__) . '/tests/codeception');
 
 $params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/db.php');
 
 $config = [
     'id' => 'basic-console',
-    'basePath' => dirname (__DIR__),
+    'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'app\commands',
     'components' => [
@@ -22,15 +22,6 @@ $config = [
                 ],
             ],
         ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'enableStrictParsing' => false,
-            'rules' => [
-                '<alias:index|about|contact>' => 'site/<alias>',
-                '<alias:login|logout|register>' => 'site/<alias>',
-            ],
-        ],
         'db' => $db,
     ],
     'params' => $params,
@@ -43,7 +34,7 @@ $config = [
          */
 ];
 
-if (YII_ENV_DEV)
+if(YII_ENV_DEV)
 {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'gii';

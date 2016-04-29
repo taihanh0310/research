b2158d4300e8e23eea61999b326f85a7ead1c5d3
Hanh Nguyen <taihanh0310@gmail.com>
2016-04-29 17:52:48 +0700

    index file

--- basic-yii-2/views/site/index.php
+++ basic-yii-2/views/site/index.php
@@ -1,97 +1,113 @@
 <?php
-/* @var $this yii\web\View */
 
+use \yii\bootstrap\Modal;
 use kartik\social\FacebookPlugin;
-use yii\bootstrap\Collapse;
-use yii\bootstrap\Alert;
+use \yii\bootstrap\Collapse;
+use \yii\bootstrap\Alert;
 use yii\helpers\Html;
-use yii\bootstrap\Modal;
-use yii\helpers\Url;
+
+/* @var $this yii\web\View */
+$this->title = 'Yii 2 Build';
 ?>
-
 <div class="site-index">
+
+    <div class="site-index">
+
     <div class="jumbotron">
-        <?php
-        if (Yii::$app->user->isGuest) {
-            echo Html::a('Get started today', ['site/signup'], ['class' => 'btn btn-lg btn-success']);
-        }
-        ?>
-        <p>
-        <h1>Yii 2 Build</h1>
-        <p class="lead">Use this Yii 2 Template to start Projects.</p>
-        </p>
-        <br/>
-        <?php
-        echo FacebookPlugin::widget(
-                [ 'type' => FacebookPlugin::LIKE,
-                    'settings' => []]);
-        ?>
-    </div>
-    <div>
-        <?php
-        echo FacebookPlugin::widget(['appId' => 'FACEBOOK_APP_ID']);
-        ?>
-    </div>
-    <?php
-    echo Collapse::widget([
-
-        'items' => [
-
-            [
-
-                'label' => 'Top Features',
-                'content' => FacebookPlugin::widget([
-                    
-                    'type' => FacebookPlugin::SHARE,
-                    'settings' => ['href' => 'http://basic-yii-2.com/', 'width' => '350']
-                ]),
-            // open its content by default
-            //'contentOptions' => ['class' => 'in']
-            ],
-            // another group item
-            [
-
-                'label' => 'Top Resources',
-                'content' => FacebookPlugin::widget([
-                    
-                    'type' => FacebookPlugin::SHARE,
-                    'settings' => ['href' => 'http://basic-yii-2.com/', 'width' => '350']
-                ]),
-            // 'contentOptions' => [],
-            // 'options' => [],
-            ],
-        ]
-    ]);
-
-
-
-    Modal::begin([
-
-        'header' => '<h2>Latest Comments</h2>',
-        'toggleButton' => ['label' => 'comments'],
-    ]);
-
-    echo FacebookPlugin::widget([
-        
-        'type' => FacebookPlugin::COMMENT,
-        'settings' => ['href' => 'http://basic-yii-2.com/', 'width' => '350']
-    ]);
-
-    Modal::end();
+    
+   <?php if (Yii::$app->user->isGuest) {
+        echo Html::a('Get Started Today', ['site/signup'], ['class' => 'btn btn-lg btn-success']);
+        } 
     ?>
+        </p>
 
-    <br/>
-    <br/>
+       
+        <h1>Yii 2 Build <i class="fa fa-plug"></i></h1>
 
-    <?Php
-    echo Alert::widget([
+        <p class="lead">Use this Yii 2 Template to start Projects.</p>
 
-        'options' => [
-            'class' => 'alert-info',
+        
+        <br/>
+
+         <?php  echo FacebookPlugin::widget(['type'=>FacebookPlugin::LIKE, 'settings' => []]); ?> 
+    
+</div>
+
+    <?php
+  
+       echo Collapse::widget([
+   
+       'items' => [
+                                       
+        [
+            
+        'label' => 'Top Features' ,
+        'content' => FacebookPlugin::widget([
+                                                    
+        'type'=>FacebookPlugin::SHARE, 
+        'settings' => ['href'=>'http://www.yii2build.com','width'=>'350']
+                                                    
+        ]),
+                        
+        // open its content by default
+        //'contentOptions' => ['class' => 'in']
+                        
         ],
-        'body' => 'Launch your project like a rocket...',
-    ]);
-    ?> 
+                        
+       // another group item
+                        
+        [
+                                                       
+         'label' => 'Top Resources',
+         'content' => FacebookPlugin::widget([
+                                                    
+         'type'=>FacebookPlugin::SHARE, 
+         'settings' => ['href'=>'http://www.yii2build.com','width'=>'350']
+                                                    
+         ]),
+                        
+         // 'contentOptions' => [],
+         // 'options' => [],
+                                                    
+         ],
+                                                    
+         ]
+                                                    
+         ]);
+
+
+  
+         Modal::begin([
+                       
+           'header' => '<h2>Latest Comments</h2>',
+           'toggleButton' => ['label' => 'comments'],
+                        
+             ]);
+
+            echo   FacebookPlugin::widget([
+                       
+            'type'=>FacebookPlugin::COMMENT, 
+            'settings' => ['href'=>'http://www.yii2build.com','width'=>'350']
+                       
+             ]); 
+
+            Modal::end();
+                        
+        ?>
+      
+<br/>
+<br/>
+        
+        <?Php
+                               
+        echo Alert::widget([
+                                
+          'options' => [
+          'class' => 'alert-info',
+           ],
+          'body' => 'Launch your project like a rocket...',
+          ]);
+        ?> 
 
     <div class="body-content">
 
@@ -100,76 +116,83 @@ use yii\helpers\Url;
                 <h2>Free</h2>
 
                 <p>
-                    <?php
-                    if (!Yii::$app->user->isGuest) {
+     <?php 
+                        
+      if (!Yii::$app->user->isGuest) {
+                                
+         echo Yii::$app->user->identity->username . ' is doing cool stuff. '; 
+      } 
+                            
+     ?> 
 
-                        echo Yii::$app->user->identity->username . ' is doing cool stuff. ';
-                    }
-                    //echo FacebookPlugin::widget(['type'=>FacebookPlugin::COMMENT, 'settings' => ['data-width'=>1000, 'data-numposts'=>5]]);
-                    echo FacebookPlugin::widget([]);
-                    ?> 
+                      
+  Starting with this free, open source Yii 2 template and it will save you 
+  a lot of time.  You can deliver projects to the customer quickly, with 
+  a  lot of boilerplate already taken care of for you, so you can concentrate 
+  on the complicated stuff.</p>
 
+<p>
 
-                    Starting with this free, open source Yii 2 template and it will save you 
-                    a lot of time.  You can deliver projects to the customer quickly, with 
-                    a  lot of boilerplate already taken care of for you, so you can concentrate 
-                    on the complicated stuff.</p>
+<a class="btn btn-default" href="http://www.yiiframework.com/doc-2.0/guide-index.html">Yii Documentation &raquo;</a>
 
-                <p>
+</p>
+                        
+      <?php  
+                            
+       echo FacebookPlugin::widget([
 
-                    <a class="btn btn-default" href="http://www.yiiframework.com/doc-2.0/guide-index.html">Yii Documentation &raquo;</a>
+       'type'=>FacebookPlugin::LIKE, 
+       'settings' => []
 
-                </p>
-
-                <?php
-                echo FacebookPlugin::widget([
-                    
-                    'type' => FacebookPlugin::LIKE,
-                    'settings' => []
-                ]);
-                ?> 
-
+       ]); 
+                            
+       ?> 
+                        
             </div>
             <div class="col-lg-4">
                 <h2>Advantages</h2>
 
-                <p>
+<p>
 
-                    Ease of use is a huge advantage.  We've simplifiled RBAC and given you Free/Paid
-                    user type out of the box.  The Social plugins are so quick and easy to install, 
-                    you will love it!
+Ease of use is a huge advantage.  We've simplifiled RBAC and given you Free/Paid
+ user type out of the box.  The Social plugins are so quick and easy to install, 
+you will love it!
 
-                </p>
+</p>
 
-                <p>
+<p>
 
-                    <a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a>
+<a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a>
 
-                </p>
-
-                <?php
-                echo FacebookPlugin::widget([
-                    'type' => FacebookPlugin::COMMENT,
-                    'settings' => ['href' => 'http://basic-yii-2.com/', 'width' => '350']
-                ]);
-                ?> 
+</p>
+                        
+      <?php   
+                            
+       echo FacebookPlugin::widget([
+                            
+         'type'=>FacebookPlugin::COMMENT, 
+         'settings' => ['href'=>'http://www.yii2build.com','width'=>'350']
+                                
+        ]); 
+                            
+        ?> 
             </div>
             <div class="col-lg-4">
                 <h2>Code Quick, Code Right!</h2>
 
-                <p>
+<p>
 
-                    Leverage the power of the awesome Yii 2 framework with this enhanced template.  
-                    Based Yii 2's advanced template, you get a full frontend and backend 
-                    implementation that features rich UI for backend management.
+Leverage the power of the awesome Yii 2 framework with this enhanced template.  
+Based Yii 2's advanced template, you get a full frontend and backend 
+implementation that features rich UI for backend management.
 
-                </p>
+</p>
 
-                <p>
+<p>
 
-                    <a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a>
+<a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a>
 
-                </p>
+</p>
 
             </div>
         </div>
